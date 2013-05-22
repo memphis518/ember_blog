47503ec107bfeb306774117da11601b04268c8bc
Scott Hamilton <shamil614@yahoo.com>
2013-05-22 10:37:23 -0500

    Posts and Post routes setup and passes obj to controllers.  with views.

--- app/assets/javascripts/controllers/posts_controller.js
+++ app/assets/javascripts/controllers/posts_controller.js
@@ -1,1 +1,4 @@
 EmberBlog.PostsController = Ember.ArrayController.extend({});
+
+// Not needed - generated automatically
+//EmberBlog.PostController = Ember.ObjectController.extend({});
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,9 +1,16 @@
 EmberBlog.Router.map(function(match){
-  this.route('posts', {path: "/" });
+  this.resource('posts');
+  this.resource('post', {path: '/post/:post_id'});
 });
 
 EmberBlog.PostsRoute = Ember.Route.extend({
   model: function() {
     return EmberBlog.Post.find()
+  }
+});
+
+EmberBlog.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return EmberBlog.Post.find(params.post_id)
   }
 });
